abdaced198928497585057e73fc05ba7fd963e57
Romebomben <hugo.romeborn@elev.ga.ntig.se>
2025-05-12 22:32:07 +0200

    Komplettering
    Har ändrat till mobile-first struktur och kommenterat koden

--- JS/Home.js
+++ JS/Home.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+// En funktion som aktiverar och deaktiverar hamburgermenyn
+document.addEventListener("DOMContentLoaded", function () { // en eventlistener som startar funktionen
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
     menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
+        navLinks.classList.toggle("active"); // sätter nav_links som active och de visas
     });
 });
--- JS/Recept.js
+++ JS/Recept.js
@@ -1,17 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
+// En funktion som aktiverar och deaktiverar hamburgermenyn
+document.addEventListener("DOMContentLoaded", function () { // en eventlistener som startar funktionen
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
     menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
+        navLinks.classList.toggle("active");    // sätter nav_links som active och de visas
     });
 });
 
-function showSection(sectionId, button) {
-    document.getElementById('ingredients').style.display = 'none';
+// Funktionen ser till att rätt sektion av receptet visas, instruktioner eller ingridienser
+function showSection(sectionId, button) {   
+    document.getElementById('ingredients').style.display = 'none';  // nollställer/gömmer båda sektionerna
     document.getElementById('instructions').style.display = 'none';
-    document.getElementById(sectionId).style.display = 'block';
+    document.getElementById(sectionId).style.display = 'block';  // visar den section man vill
 
-    document.querySelectorAll('.toggle-button').forEach(btn => btn.classList.remove('active'));
+    document.querySelectorAll('.toggle-button').forEach(btn => btn.classList.remove('active')); // ändrar utseende för knappen så att den markerar vilken som visas
     button.classList.add('active');
 }
